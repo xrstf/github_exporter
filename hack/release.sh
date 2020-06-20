162f08c397fda69eb8826652648916f6f737c33f
xrstf <git@xrstf.de>
2020-06-20 02:02:48 +0200

    add release helper script to provide a sensible k8s manifest for tagged releases

---
+++ hack/release.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cd $(dirname $0)/..
+
+version="${1:-}"
+version=${version#"v"}
+
+if [ -z "$version" ]; then
+  echo "Usage: $0 VERSION"
+  echo "Hint: Version prefix 'v' is automatically trimmed."
+  exit 1
+fi
+
+if git tag | grep "v$version" >/dev/null; then
+  echo "Version is already tagged."
+  exit 1
+fi
+
+set_version() {
+  yq write -i contrib/kubernetes/deployment.yaml 'spec.template.spec.containers[0].image' "xrstf/github_exporter:$1"
+}
+
+set_version "$version"
+git commit -am "version $version"
+git tag -m "version $version" "v$version"
+
+set_version "latest"
+git commit -am "back to dev"
